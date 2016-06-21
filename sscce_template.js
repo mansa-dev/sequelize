928eda51968a1780fc50d21753c34a7df919f652
Sushant <sushantdhiman@outlook.com>
2016-06-21 11:57:02 +0530

    jshint the sscce template

--- sscce_template.js
+++ sscce_template.js
@@ -1,3 +1,5 @@
+'use strict';
+
 /*
  * Copy this file to ./sscce.js
  * Add code from issue
@@ -6,24 +8,3 @@
 
 var Sequelize = require('./index');
 var sequelize = require('./test/support').createSequelizeInstance();
-
-let User = sequelize.define('User', {
-  name: Sequelize.STRING,
-  surname: Sequelize.STRING
-});
-
-return sequelize.sync({ force: true })
-.then(() => (
-  User.bulkCreate([
-    { name: 'Hakue', surname: 'Shake'},
-    { name: 'Parse karo', surname: 'ddsd'},
-    { name: 'Parse karo', surname: 'dssd'}
-  ])
-))
-.then(() => {
-  return User.findAll({
-    where: {
-      name: 'Parse karo'
-    }
-  });
-});
